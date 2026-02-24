28178e6e57d3acb7b9bf5345677e1df1721d4135
28EMcCallum <28EMcCallum@BishopFoley.org>
2026-02-23 21:12:11 -0500

    started bringing in vision data based off of slack format

---
+++ src/main/java/frc/robot/VisionConstants.java
@@ -0,0 +1,13 @@
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Pose3d;
+
+public class VisionConstants {
+    public double fuelNum = 0; //TODO: actually bring in from vision AND add correct types
+    public double piTime = 0;
+    public double piProcessTime = 0;
+    public double fuelDist =0;
+    public double fuelVisAngle = 0;
+    public double groupNumVis = 0;
+    public double groupOrientVis = 0;
+}

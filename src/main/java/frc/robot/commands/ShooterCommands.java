a71e6609fdb7d1a3ae3da9cfa63f3f0d940c5e96
Roboprog <roboprog@BishopFoley.org>
2026-03-07 13:35:05 -0500

    refactored smarter shoot

--- src/main/java/frc/robot/commands/ShooterCommands.java
+++ src/main/java/frc/robot/commands/ShooterCommands.java
@@ -1,13 +1,16 @@
 package frc.robot.commands;
 
+import edu.wpi.first.math.filter.Debouncer;
 import edu.wpi.first.math.filter.Debouncer.DebounceType;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.FieldConstants;
 import frc.robot.RobotContainer;
 import java.util.function.Consumer;
@@ -63,29 +66,37 @@ public class ShooterCommands {
         Thing<Double> velocityThing = new Thing<>(); // thing2
 
         // complex use of debounce, but we are looking for if time has elapsed since shooting a ball
-        Trigger ballShotTrigger =
-                new Trigger(() -> r.shooter.ballShotEdge).debounce(0.75, DebounceType.kFalling);
+        Debouncer shotDebounce = new Debouncer(0.75, DebounceType.kFalling);
 
-        return new RunCommand(
+        // index sequence
+        SequentialCommandGroup indexerSequence = new SequentialCommandGroup();
+        // first reset debouncer as if we have just made a shot
+        indexerSequence.addCommands(new InstantCommand(() -> shotDebounce.calculate(true)));
+        // then run indexer until it gets jammed
+        indexerSequence.addCommands(
+                r.spindexter
+                        .smartSpinCmd(r.shooter, r.drive)
+                        .until(() -> shotDebounce.calculate(r.shooter.ballShotEdge)));
+        // then run the unjam sequence
+        indexerSequence.addCommands(r.spindexter.smartUnjam().withDeadline(new WaitCommand(0.5)));
+
+        // run shoot, drive, and index in parallel
+        ParallelCommandGroup parallelGroup = new ParallelCommandGroup();
+        parallelGroup.addCommands(
+                new RunCommand(
                         () ->
                                 r.shooter.newPrime(
                                         target, r.drive.getPose(), rotationThing, velocityThing),
-                        r.shooter)
-                .alongWith(
-                        DriveCommands.driveAtAngleFFw(
-                                r.drive,
-                                () -> -controller.getLeftY(),
-                                () -> -controller.getLeftX(),
-                                rotationThing,
-                                velocityThing))
-                .alongWith(
-                        r.spindexter
-                                .smartSpinCmd(r.shooter, r.drive)
-                                .until(ballShotTrigger)
-                                .andThen(
-                                        r.spindexter
-                                                .smartUnjam()
-                                                .withDeadline(new WaitCommand(0.5)))
-                                .repeatedly());
+                        r.shooter));
+        parallelGroup.addCommands(
+                DriveCommands.driveAtAngleFFw(
+                        r.drive,
+                        () -> -controller.getLeftY(),
+                        () -> -controller.getLeftX(),
+                        rotationThing,
+                        velocityThing));
+        parallelGroup.addCommands(indexerSequence.repeatedly());
+
+        return parallelGroup;
     }
 }

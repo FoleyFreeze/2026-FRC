fd2cc183406e21459cfc7bce775fd9bc48fa59d3
Roboprog <roboprog@BishopFoley.org>
2026-03-29 15:44:31 -0400

    shorter trench auto 100rpm less on 4-5m shots

--- src/main/java/frc/robot/subsystems/shooter/ShooterInterp1d.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterInterp1d.java
@@ -15,7 +15,7 @@ public class ShooterInterp1d {
             double rpm, double angle, double hood, double time, double turretVel, double dist) {}
 
     // based on quick testing
-    private static final double[] rpmTableReal = {2500, 2700, 3050, 3200, 3400, 3600, 3800, 4000};
+    private static final double[] rpmTableReal = {2500, 2700, 3050, 3200, 3300, 3500, 3700, 4000};
     private static final double[] hoodAngleTableReal = {70, 55, 50.5, 50.5, 50.5, 50.5, 50.5, 50.5};
     private static final double[] timeTableReal = {0.58, 0.78, 0.91, 1.04, 1.30, 1.37, 1.44, 1.50};
     private static final double[] distAxisReal = {1.317, 2.75, 3.54, 3.91, 4.42, 5.03, 5.18, 5.79};

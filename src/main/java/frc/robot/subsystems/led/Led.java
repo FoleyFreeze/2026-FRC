a97f0d8f674d79bde195c8032bf9971766b68fc8
Roboprog <roboprog@BishopFoley.org>
2026-04-15 22:22:47 -0400

    fix leds, deploy latest elastic layout to robot

--- src/main/java/frc/robot/subsystems/led/Led.java
+++ src/main/java/frc/robot/subsystems/led/Led.java
@@ -4,6 +4,7 @@ import static edu.wpi.first.units.Units.Percent;
 import static edu.wpi.first.units.Units.Seconds;
 
 import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLED.ColorOrder;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
 import edu.wpi.first.wpilibj.LEDPattern;
 import edu.wpi.first.wpilibj.util.Color;
@@ -26,6 +27,7 @@ public class Led extends SubsystemBase {
         BLINK_BLUE(LEDPattern.solid(Color.kBlue).blink(Seconds.of(1), Seconds.of(1))),
         BREATHE_BLUE(LEDPattern.solid(Color.kBlue).breathe(Seconds.of(3))),
         WHITE(LEDPattern.solid(Color.kWhite).atBrightness(Percent.of(100))),
+        YELLOW(LEDPattern.solid(Color.kYellow).atBrightness(Percent.of(100))),
 
         RAINBOW(LEDPattern.rainbow(255, 255).scrollAtRelativeSpeed(Seconds.of(5).asFrequency()));
 
@@ -39,11 +41,19 @@ public class Led extends SubsystemBase {
     public Led(RobotContainer r) {
         this.r = r;
         leds = new AddressableLED(9);
-        buffer = new AddressableLEDBuffer(40);
+        // leds.setSyncTime(280);
+        // leds.setBitTiming(0,0,0,0);
+        leds.setColorOrder(ColorOrder.kRGB);
+        buffer = new AddressableLEDBuffer(49);
+
+        leds.setLength(buffer.getLength());
+        leds.start();
     }
 
     @Override
-    public void periodic() {}
+    public void periodic() {
+        leds.setData(buffer);
+    }
 
     public Command setLEDMode(LED_MODES mode) {
         Command c = new RunCommand(() -> mode.pattern.applyTo(buffer), this).ignoringDisable(true);
